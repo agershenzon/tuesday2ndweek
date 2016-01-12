8f48120b50153a1666b9e1146aa0b6c7a3112173
ag <aarongershenzon@gmail.com>
2016-01-12 14:10:12 -0800

    finished madlib site

--- madlib/js/scripts.js
+++ madlib/js/scripts.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
-  $("button#bark").click(function() {
-    $("ul#dog").prepend("<li>Bark!!!</li>");
-  });
+  $('#blanks form').submit(function(event){
+    var person1Input= $("input#person1").val();
+    var person2Input= $("input#person2").val();
+    var animalInput= $("input#animal").val();
+    var exclamationInput= $("input#exclamation").val();
+    var verbInput= $("input#verb").val();
+    var nounInput= $("input#noun").val();
 
-  $("button#meow").click(function() {
-    $("ul#cat").prepend("<li>Meow!!!</li>");
-  });
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
 
+    $('#story').show();
+    event.preventDefault();
+
+  });
 });
